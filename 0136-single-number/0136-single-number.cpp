edf24a04d5b7fcf0ff4dbf6c1c36173a3adeb635
Abhishekcod1e <abhishek09rkumar@gmail.com>
2026-08-09 22:25:42 +0530

    Time: 0 ms (100%), Space: 20.6 MB (84.42%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int,int>mpp;
-        for(int i = 0;i<nums.size();i++){
-            mpp[nums[i]]++;
+        int k=0;
+        for(int i=0;i<nums.size();i++){
+            k^=nums[i];
         }
-        for(auto it: mpp){
-            if(it.second == 1)
-            return it.first;
-        }
-        return 0;
+        return k;
     }
 };
